937bb3e2d4dc459cccd8aeac8cc2e16c47095d71
rockwolf <thereisanewway@gmail.com>
2016-12-18 00:09:24 +0100

    Added hostname to connection.

--- src/config.lisp
+++ src/config.lisp
@@ -20,7 +20,7 @@
 (defparameter *template-directory* (merge-pathnames #P"templates/" *application-root*))
 
 (defconfig :common
-  `(:databases ((:maindb :sqlite3 :database-name ":memory:"))))
+  `(:databases ((:maindb :postgres :database-name "finance" :username "rockwolf" :host "debby_test"))))
 
 (defconfig |development|
   '())
